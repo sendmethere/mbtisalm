200bcd8ff717b8281d3fb8578de17e26d59e6eb5
행복한엄쌤 <sendmethere@naver.com>
2024-06-30 17:14:07 +0900

    _

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const Home = () => (
-  <div>
-    <p>Main Scene</p>
+  <div className='h-[400px] flex justify-center items-center'>
+    <p className='font-black'>Main Scene</p>
   </div>
 );
 
